2c6a0d59cd53df065f92b2ccc342db70730339b8
om verma <131939085+anthanma@users.noreply.github.com>
2026-03-09 00:02:57 +0530

    Create max_consecutive_one-3.java

---
+++ day3/max_consecutive_one-3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int countZero=0;
+        int left=0;
+        int maxLen=0;
+        for(int right=0; right<nums.length;right++){
+            if(nums[right]==0){
+                countZero++;
+            }
+            while(countZero>k){
+                if(nums[left]==0){
+                    countZero--;
+                }
+                left++;
+            }
+            maxLen=Math.max(maxLen,right-left+1);
+
+        }
+        return maxLen;
+    }
+}
