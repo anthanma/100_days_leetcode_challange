89755a4ef25126a044741236ed71eae842291cbd
om verma <131939085+anthanma@users.noreply.github.com>
2026-03-14 12:52:21 +0530

    Create postorderTreversal.java

---
+++ day8/postorderTreversal.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        TreeNode node = root;
+        helper(ans, node);
+        return ans;
+    }
+    public void helper(List<Integer> ans ,TreeNode node){
+        if(node == null){
+            return;
+        }
+        // we know in pre order treversal we first take left right root  
+        
+        helper(ans,node.left);
+        helper(ans,node.right);
+        ans.add(node.val);
+    }
+}
