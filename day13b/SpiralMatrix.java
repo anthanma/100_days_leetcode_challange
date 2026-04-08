280a0dd9d147b55f8456d964890a68544ead8a60
om verma <131939085+anthanma@users.noreply.github.com>
2026-04-08 21:03:16 +0530

    Create SpiralMatrix.java

---
+++ day13b/SpiralMatrix.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        int rows=matrix.length;
+        int cols=matrix[0].length;
+        int top=0;
+        int bottom=rows-1;
+        int left=0;
+        int right=cols-1;
+        
+        while(left<=right && top<=bottom){
+            // move left to right in top wall 
+            for(int i=left;i<=right;i++){
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom; i++){
+                ans.add(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right; i>=left; i--){
+                ans.add(matrix[bottom][i]);
+                
+            }
+            bottom--;
+
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                ans.add(matrix[i][left]);
+            }
+            left++;
+
+            }
+        }
+        return ans;
+    }
+}
